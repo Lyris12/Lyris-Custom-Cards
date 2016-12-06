a75e19d5b0de0d3eff34f4dad5fbb353c39f057d
Lyris <claragracepaulsen@gmail.com>
2016-12-06 16:27:16 -0500

    Finished "Fate's Dainty Diaduke"

---
+++ script/c101010090.lua
@@ -0,0 +1,44 @@
+--created & coded by Lyris
+--Fate's Dainty Diaduke
+function c101010090.initial_effect(c)
+	--If this card is Special Summoned from the hand or with a "Fate's" card: "Fate's" monsters cannot be targeted or destroyed by card effects for the rest of this turn. If this card was Pendulum Summoned, you must have a "Fate's" card in your Pendulum Zone to activate this effect.
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCondition(c101010090.condition)
+	e1:SetOperation(c101010090.operation)
+	c:RegisterEffect(e1)
+end
+function c101010090.condition(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local pc1=Duel.GetFieldCard(tp,LOCATION_SZONE,6)
+	local pc2=Duel.GetFieldCard(tp,LOCATION_SZONE,7)
+	return (bit.band(c:GetSummonType(),SUMMON_TYPE_PENDULUM)==SUMMON_TYPE_PENDULUM and (pc1:IsSetCard(0xf7a) or pc2:IsSetCard(0xf7a))) or c:GetSummonLocation()==LOCATION_HAND
+end
+function c101010090.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0xf7a)
+end
+function c101010090.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(c101010090.filter,tp,LOCATION_MZONE,0,nil)
+	local tc=g:GetFirst()
+	while tc do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,2)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e2:SetValue(1)
+		e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,2)
+		tc:RegisterEffect(e2)
+		tc=g:GetNext()
+	end
+end
