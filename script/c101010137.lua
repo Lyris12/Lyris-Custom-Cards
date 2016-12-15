364c1f7318136a571b3bf4962fdcd06084bf07c4
Lyris <claragracepaulsen@gmail.com>
2016-12-14 19:22:27 -0500

    Scripted "Crystapal" cards

---
+++ script/c101010137.lua
@@ -0,0 +1,60 @@
+--Crystapal Avzin the Diamond
+function c101010137.initial_effect(c)
+	aux.EnablePendulumAttribute(c)
+	--If a player draws a Trap Card(s): that player reveals that card(s); excavate cards from the top of your Deck until you excavate a "Liquid Crystapal" monster, Special Summon that card to that player's field, then repeat this effect up to a number of times equal to the number of cards they revealed - 1. After this effect resolves, shuffle all excavated cards not Summoned by that effect into the Deck.
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_DRAW)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCondition(c101010137.spcon)
+	e2:SetTarget(c101010137.sumtg)
+	e2:SetOperation(c101010137.sumop)
+	c:RegisterEffect(e2)
+end
+function c101010137.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return not eg:IsExists(Card.IsControler,1,nil,1-eg:GetFirst():GetControler())
+end
+function c101010137.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local ap=eg:GetFirst():GetControler()
+	Duel.ConfirmCards(1-ap,eg)
+	Duel.ShuffleHand(ap)
+	e:SetLabel(eg:GetCount())
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,PLAYER_NONE,LOCATION_DECK)
+end
+function c101010137.filter(c,e,tp)
+	return c:IsSetCard(0x1613) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c101010137.sumop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local p=c:GetControler()
+	local ap=eg:GetFirst():GetControler()
+	local sg=Duel.GetMatchingGroup(c101010137.filter,p,LOCATION_DECK,0,nil,e,ap)
+	local ft1=Duel.GetLocationCount(ap,LOCATION_MZONE)
+	local ct=e:GetLabel()
+	if ft1>0 then
+		local dcount=Duel.GetFieldGroupCount(p,LOCATION_DECK,0)
+		if sg:GetCount()==0 then
+			Duel.ConfirmDecktop(p,dcount)
+			Duel.ShuffleDeck(p)
+			return
+		end
+		if ct>ft1 then ct=ft1 end
+		local dc=nil
+		while dcount>0 and ct>0 do
+			Duel.ConfirmDecktop(p,1)
+			dc=Duel.GetDecktopGroup(p,1):GetFirst()
+			if sg:IsContains(dc) then
+				Duel.SpecialSummonStep(dc,0,ap,ap,false,false,POS_FACEUP)
+				ct=ct-1
+			else
+				Duel.MoveSequence(dc,1)
+			end
+			dcount=dcount-1
+		end
+		Duel.SpecialSummonComplete()
+	end
+end
---
+++ script/c101010138.lua
@@ -0,0 +1,56 @@
+--Crystapal Hiro the Pearl
+function c101010138.initial_effect(c)
+	aux.EnablePendulumAttribute(c)
+	--After the effect of a Spell Card in a player's Spell & Trap Zone resolves: Excavate cards from the top of your Deck until you excavate a "Liquid Crystapal" monster, that player Special Summons that card to their field, also, shuffle all excavated cards not Summoned by this effect back into the Deck.
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_CHAIN_SOLVED)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetOperation(c101010138.chop1)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_CHAIN_END)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetOperation(c101010138.sumcon)
+	e2:SetTarget(c101010138.sumtg)
+	e2:SetOperation(c101010138.sumop)
+	c:RegisterEffect(e2)
+end
+function c101010138.sumcon(e,tp,eg,ep,ev,re,r,rp)
+	if not re:IsActiveType(TYPE_SPELL) or re:GetActivateLocation()~=LOCATION_SZONE then return end
+	e:GetLabelObject():SetLabelObject(re:GetHandler())
+	return re:GetHandler():GetSequence()<=4
+end
+function c101010138.filter(c,e,tp)
+	return c:IsSetCard(0x1613) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c101010138.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c101010138.filter,e:GetHandler():GetControler(),LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function c101010138.sumop
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local g=Duel.GetMatchingGroup(Card.IsSetCard,tp,LOCATION_DECK,0,nil,0x1613)
+	local dcount=Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)
+	if dcount==0 then return end
+	if g:GetCount()==0 then
+		Duel.ConfirmDecktop(tp,dcount)
+		Duel.ShuffleDeck(tp)
+		return
+	end
+	local seq=-1
+	local tc=g:GetFirst()
+	local spcard=nil
+	while tc do
+		if tc:GetSequence()>seq then 
+			seq=tc:GetSequence()
+			spcard=tc
+		end
+		tc=g:GetNext()
+	end
+	Duel.ConfirmDecktop(tp,dcount-seq)
+	Duel.SpecialSummon(spcard,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+end
