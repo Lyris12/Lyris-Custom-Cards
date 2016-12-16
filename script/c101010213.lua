40d4245330fe909fedf2d28b52d109266b74eeea
Lyris <claragracepaulsen@gmail.com>
2016-12-16 15:51:28 -0500

    Scripted "Clear Trojan"

---
+++ script/c101010213.lua
@@ -0,0 +1,41 @@
+--Clear Trojan
+function c101010213.initial_effect(c)
+	--Activate Special Summon 1 "Clear" monster from your Deck to your opponent's field, but its ATK becomes 0, and it cannot be destroyed by battle or card effects.
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetTarget(c101010213.target)
+	e1:SetOperation(c101010213.activate)
+	c:RegisterEffect(e1)
+end
+c101010213.list={[33900648]=true,[97811903]=true}
+function c101010213.filter(c,e,tp)
+	return (c:IsSetCard(0x306) or c101010213.list[c:GetCode()]) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,1-tp)
+end
+function c101010213.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c101010213.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function c101010213.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(1-tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c101010213.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if tc and Duel.SpecialSummonStep(tc,0,tp,1-tp,false,false,POS_FACEUP) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(0)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+		e2:SetValue(1)
+		tc:RegisterEffect(e2)
+		local e3=e2:Clone()
+		e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+		tc:RegisterEffect(e3)
+	end
+end
