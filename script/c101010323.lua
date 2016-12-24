1073dbcb470882ce74abda2ec3e3349130cb69de
Lyris <claragracepaulsen@gmail.com>
2016-12-24 14:34:03 -0500

    Scripted "Victorious Determination"; tweaked "Whirlwinder's Updraft"

---
+++ script/c101010323.lua
@@ -0,0 +1,28 @@
+--Victorious Determination
+function c101010323.initial_effect(c)
+	--All "Victorial" monsters you control gain 500 ATK.
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(TIMING_DAMAGE_STEP)
+	e1:SetCondition(c101010323.condition)
+	c:RegisterEffect(e1)
+	--atk up
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x50b))
+	e2:SetValue(500)
+	c:RegisterEffect(e2)
+	--"Victorial" monsters you control cannot be targeted by your opponent's card effects.
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e3:SetValue(aux.tgoval)
+	c:RegisterEffect(e3)
+end
+function c101010323.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
+end
