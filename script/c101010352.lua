5f9156a8cfc4947d8c8077027f3def3a99e7789a
Lyris <claragracepaulsen@gmail.com>
2016-12-24 20:52:17 -0500

    Scripted "Destiny HERO Loyal Prince", removed "Soul Inversion Channel" cards

---
+++ script/c101010352.lua
@@ -0,0 +1,47 @@
+--Destiny HERO Loyal Prince
+function c101010352.initial_effect(c)
+	--If this card is Normal or Special Summoned: You can target 1 Level 4 "Destiny HERO" monster in your Graveyard, except "Destiny HERO Loyal Prince"; Special Summon it, but it cannot be used as an Xyz Material this turn.
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e1:SetTarget(c101010352.target)
+	e1:SetOperation(c101010352.operation)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	--control
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_CANNOT_CHANGE_CONTROL)
+	c:RegisterEffect(e3)
+end
+function c101010352.filter(c,e,tp)
+	return c:GetLevel()==4 and c:IsSetCard(0xc008) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and not c:IsCode(101010352)
+end
+function c101010352.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c101010352.filter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(c101010352.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local tc=Duel.SelectTarget(tp,c101010352.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,tc,1,0,0)
+end
+function c101010352.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_BE_XYZ_MATERIAL)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		e1:SetValue(1)
+		tc:RegisterEffect(e1)
+		Duel.SpecialSummonComplete()
+	end
+end
