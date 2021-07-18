Lyris12/Lyris-Cards

Restart repository history

script/c102400013.lua
@@ -1,0 +1,83 @@
+--created & coded by Lyris, art from "Vylon Element" & at https://us.123rf.com/450wm/racener/racener1605/racener160500001/56306432-abstract-blue-luminous-sound-waves-background-for-parties-clubs-discos-and-concerts-.jpg?ver=6
+--インライトメント・金色ヘッドホン
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(1109)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(function(e,c) return c:IsSetCard(0xda6) and c~=e:GetHandler() end)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,id)
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetCondition(s.descon)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
+	c:RegisterEffect(e3)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsDiscardable() end
+	Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
+end
+function s.filter(c)
+	return c:IsCode(CARD_INLIGHTENED_PSYCHIC_HELMET) and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tg=Duel.GetFirstMatchingCard(s.filter,tp,LOCATION_DECK,0,nil)
+	if tg then
+		Duel.SendtoHand(tg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tg)
+	end
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	if d and d:IsControler(tp) then a,d=d,a end
+	return a:IsSetCard(0xda6) and a~=e:GetHandler()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.filter1(c)
+	return c:GetLevel()==1 and c:IsSetCard(0xda6) and c:IsAbleToHand()
+end
+function s.filter2(c)
+	return c:IsSetCard(0x1da6) and c:IsAbleToHand()
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local dir=Duel.GetAttackTarget()==nil
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_DECK,0,1,1,nil)
+	if Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
+		Duel.ConfirmCards(1-tp,g)
+		local mg=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_DECK,0,nil)
+		if dir and #mg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			local sg=mg:Select(tp,1,1,nil)
+			Duel.SendtoHand(sg,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,sg)
+		end
+	end
+end

script/c102400016.lua
@@ -1,0 +1,85 @@
+--created & coded by Lyris, art from Yu-Gi-Oh! Duel Monsters Episode 156
+--インライトメント・アルティマ ケースト
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e0:SetValue(aux.FALSE)
+	c:RegisterEffect(e0)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(1,0)
+	e2:SetCondition(s.condition)
+	c:RegisterEffect(e2)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e4:SetCategory(CATEGORY_TOGRAVE+CATEGORY_REMOVE)
+	e4:SetTarget(s.sgtg)
+	e4:SetOperation(s.sgop)
+	c:RegisterEffect(e4)
+	c:SetUniqueOnField(1,0,aux.FilterBoolFunction(Card.IsSetCard,0x1da6),LOCATION_MZONE)
+end
+function s.spfilter(c,g,ft,tp)
+	if c:IsControler(tp) and c:GetSequence()<5 then ft=ft+1 end
+	return c:IsCode(id-9) and (c:IsControler(tp) or c:IsFaceup())
+		and (ft>0 or g:IsExists(s.mzfilter,1,c,tp))
+end
+function s.mzfilter(c,tp)
+	return c:IsControler(tp) and c:GetSequence()<5
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local rg=Duel.GetReleaseGroup(tp):Filter(Card.IsSetCard,nil,0xda6)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	return ft>-2 and #rg>1 and rg:IsExists(s.spfilter,1,nil,rg,ft,tp)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local rg=Duel.GetReleaseGroup(tp):Filter(Card.IsSetCard,nil,0xda6)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local g1=rg:FilterSelect(tp,s.spfilter,1,1,nil,rg,ft,tp)
+	local tc=g1:GetFirst()
+	if tc:IsControler(tp) and tc:GetSequence()<5 then ft=ft+1 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	if ft>0 then
+		local g2=rg:Select(tp,1,1,tc)
+		g1:Merge(g2)
+	else
+		local g2=rg:FilterSelect(tp,s.mzfilter,1,1,tc,tp)
+		g1:Merge(g2)
+	end
+	Duel.Release(g1,REASON_COST)
+end
+function s.condition(e)
+	return Duel.GetAttackTarget()==nil
+end
+function s.sgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_SZONE)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,#g,0,0)
+end
+function s.sgop(e,tp,eg,ep,ev,re,r,rp)
+	local dir=Duel.GetAttackTarget()==nil
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_SZONE)
+	local ct=Duel.SendtoGrave(g,REASON_EFFECT)
+	local sg=Duel.GetOperatedGroup():Filter(Card.IsLocation,nil,LOCATION_GRAVE)
+	if ct>0 and #g>0 and dir then
+		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+	end
+end

Lyris12/Lyris-Custom-Cards

script/c102400013.lua
@@ -1,0 +1,84 @@
+--created & coded by Lyris, art from "Vylon Element" & at https://us.123rf.com/450wm/racener/racener1605/racener160500001/56306432-abstract-blue-luminous-sound-waves-background-for-parties-clubs-discos-and-concerts-.jpg?ver=6
+--インライトメント・金色ヘッドホン
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(1109)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(function(e,c) return c:IsSetCard(0xda6) and c~=e:GetHandler() end)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_BATTLED)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,id)
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetDescription(1109)
+	e3:SetCondition(s.descon)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
+	c:RegisterEffect(e3)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsDiscardable() end
+	Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
+end
+function s.filter(c)
+	return c:IsCode(CARD_INLIGHTENED_PSYCHIC_HELMET) and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tg=Duel.GetFirstMatchingCard(s.filter,tp,LOCATION_DECK,0,nil)
+	if tg then
+		Duel.SendtoHand(tg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tg)
+	end
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	if d and d:IsControler(tp) then a,d=d,a end
+	return a:IsSetCard(0xda6) and a~=e:GetHandler()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.filter1(c)
+	return c:GetLevel()==1 and c:IsSetCard(0xda6) and c:IsAbleToHand()
+end
+function s.filter2(c)
+	return c:IsSetCard(0x1da6) and c:IsAbleToHand()
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local dir=Duel.GetAttackTarget()==nil
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_DECK,0,1,1,nil)
+	if Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
+		Duel.ConfirmCards(1-tp,g)
+		local mg=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_DECK,0,nil)
+		if dir and #mg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			local sg=mg:Select(tp,1,1,nil)
+			Duel.SendtoHand(sg,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,sg)
+		end
+	end
+end

script/c102400016.lua
@@ -1,0 +1,86 @@
+--created & coded by Lyris, art from Yu-Gi-Oh! Duel Monsters Episode 156
+--インライトメント・アルティマ ケースト
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e0:SetValue(aux.FALSE)
+	c:RegisterEffect(e0)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(1,0)
+	e2:SetCondition(s.condition)
+	c:RegisterEffect(e2)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_BATTLED)
+	e4:SetCategory(CATEGORY_TOGRAVE+CATEGORY_REMOVE)
+	e4:SetDescription(1100)
+	e4:SetTarget(s.sgtg)
+	e4:SetOperation(s.sgop)
+	c:RegisterEffect(e4)
+	c:SetUniqueOnField(1,0,aux.FilterBoolFunction(Card.IsSetCard,0x1da6),LOCATION_MZONE)
+end
+function s.spfilter(c,g,ft,tp)
+	if c:IsControler(tp) and c:GetSequence()<5 then ft=ft+1 end
+	return c:IsCode(id-9) and (c:IsControler(tp) or c:IsFaceup())
+		and (ft>0 or g:IsExists(s.mzfilter,1,c,tp))
+end
+function s.mzfilter(c,tp)
+	return c:IsControler(tp) and c:GetSequence()<5
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local rg=Duel.GetReleaseGroup(tp):Filter(Card.IsSetCard,nil,0xda6)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	return ft>-2 and #rg>1 and rg:IsExists(s.spfilter,1,nil,rg,ft,tp)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local rg=Duel.GetReleaseGroup(tp):Filter(Card.IsSetCard,nil,0xda6)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local g1=rg:FilterSelect(tp,s.spfilter,1,1,nil,rg,ft,tp)
+	local tc=g1:GetFirst()
+	if tc:IsControler(tp) and tc:GetSequence()<5 then ft=ft+1 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	if ft>0 then
+		local g2=rg:Select(tp,1,1,tc)
+		g1:Merge(g2)
+	else
+		local g2=rg:FilterSelect(tp,s.mzfilter,1,1,tc,tp)
+		g1:Merge(g2)
+	end
+	Duel.Release(g1,REASON_COST)
+end
+function s.condition(e)
+	return Duel.GetAttackTarget()==nil
+end
+function s.sgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_SZONE)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,#g,0,0)
+end
+function s.sgop(e,tp,eg,ep,ev,re,r,rp)
+	local dir=Duel.GetAttackTarget()==nil
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_SZONE)
+	local ct=Duel.SendtoGrave(g,REASON_EFFECT)
+	local sg=Duel.GetOperatedGroup():Filter(aux.NecroValleyFilter(Card.IsLocation),nil,LOCATION_GRAVE)
+	if ct>0 and #g>0 and dir then
+		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+	end
+end